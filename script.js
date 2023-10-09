9eab02243cfe92a28429cfc8ac283bfcfe95ffe6
Brandon <143155030+Good-Day-2-U@users.noreply.github.com>
2023-10-09 10:59:19 -0700

    Create player input and game loop func for 5 games at once

--- script.js
+++ script.js
@@ -16,10 +16,60 @@ let ai = function() {
   }
 }
 
+// let aiOutcome = ai()
 
 
+let playerInput = prompt("Please choose Rock, Paper or Scissors please")
 
+// const playerInput = "rock"
+
+let game = function(a, b) {
+  if (a.toLowerCase() === b.toLowerCase()) {
+    return "It is a tie!"
+  } else if ((a.toLowerCase() == "paper") && (b.toLowerCase() == "rock")) {
+    return "You Lose! Paper beats Rock"
+  } else if ((a.toLowerCase() == "rock") && (b.toLowerCase() == "paper")) {
+    return "You Win! Paper beats Rock"
+  } else if ((a.toLowerCase() == "rock") && (b.toLowerCase() == "scissors")) {
+    return "You Lose! Rock beats Scissors"
+  } else if ((a.toLowerCase() == "scissors") && (b.toLowerCase() == "rock")) {
+    return "You Win! Rock beats Scissors"
+  } else if ((a.toLowerCase() == "paper") && (b.toLowerCase() == "scissors")) {
+    return "You Win! Scissors beats Paper"
+  } else if ((a.toLowerCase() == "scissors") && (b.toLowerCase() == "paper")) {
+    return "You Lose! Scissors beats Paper"
+  } else {
+    return "ERROR"
+  }
+}
+
+let gameOutput
 
 let aiOutcome = ai()
 
-console.log(aiOutcome)
+
+const rounds = 5
+
+for (let i = 0; i < rounds; i++) {
+  
+  let aiOutcome = ai()
+
+  gameOutput = game(aiOutcome, playerInput)
+
+  console.log(aiOutcome)
+
+  console.log(playerInput)
+
+  console.log(gameOutput)
+
+}
+
+
+// A
+// console.log(aiOutcome)
+
+//B
+// console.log(playerInput)
+
+//Game outcome
+// console.log(gameOutput)
